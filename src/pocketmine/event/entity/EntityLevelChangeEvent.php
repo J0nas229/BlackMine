e4bb86bcd0f81888ad644053310de405f0f2f5ab
caspervanneck <wesleyvanneck1012@gmail.com>
2017-04-18 18:40:24 +0200

    Add files via upload

--- src/pocketmine/event/entity/EntityLevelChangeEvent.php
+++ src/pocketmine/event/entity/EntityLevelChangeEvent.php
@@ -1,28 +1,23 @@
 <?php
 
-/*
+/**
  *
- *  _                       _           _ __  __ _
- * (_)                     (_)         | |  \/  (_)
- *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___
- * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \
- * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/
- * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___|
- *                     __/ |
- *                    |___/
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
- * This program is a third party build by ImagicalMine.
- *
- * PocketMine is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author ImagicalMine Team
- * @link http://forums.imagicalmine.net/
+ * @author PocketMine Team
+ * @link   http://www.pocketmine.net/
  *
  *
-*/
+ */
 
 namespace pocketmine\event\entity;
 
@@ -30,27 +25,23 @@ use pocketmine\entity\Entity;
 use pocketmine\event\Cancellable;
 use pocketmine\level\Level;
 
-class EntityLevelChangeEvent extends EntityEvent implements Cancellable
-{
-    public static $handlerList = null;
+class EntityLevelChangeEvent extends EntityEvent implements Cancellable{
+	public static $handlerList = null;
 
-    private $originLevel;
-    private $targetLevel;
+	private $originLevel;
+	private $targetLevel;
 
-    public function __construct(Entity $entity, Level $originLevel, Level $targetLevel)
-    {
-        $this->entity = $entity;
-        $this->originLevel = $originLevel;
-        $this->targetLevel = $targetLevel;
-    }
+	public function __construct(Entity $entity, Level $originLevel, Level $targetLevel){
+		$this->entity = $entity;
+		$this->originLevel = $originLevel;
+		$this->targetLevel = $targetLevel;
+	}
 
-    public function getOrigin()
-    {
-        return $this->originLevel;
-    }
+	public function getOrigin(){
+		return $this->originLevel;
+	}
 
-    public function getTarget()
-    {
-        return $this->targetLevel;
-    }
+	public function getTarget(){
+		return $this->targetLevel;
+	}
 }
