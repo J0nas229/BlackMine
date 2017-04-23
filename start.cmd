9416de8fbbf7e124430083b13dd9ff8acb53910c
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-04-23 19:08:27 +0200

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,5 +1,5 @@
 @echo off
-TITLE ImagicalMine - a third-party build of PocketMine-MP and server software for Minecraft: Pocket Edition
+TITLE ImagicalMine server software for Minecraft: Pocket Edition
 cd /d %~dp0
 
 if exist bin\php\php.exe (
@@ -9,22 +9,25 @@ if exist bin\php\php.exe (
 	set PHP_BINARY=php
 )
 
-if exist ImagicalMine.phar (
-	set IMAGICALMINE_FILE=ImagicalMine.phar
+if exist ImagicalMine*.phar (
+	set POCKETMINE_FILE=ImagicalMine*.phar
 ) else (
 	if exist PocketMine-MP.phar (
-		set IMAGICALMINE_FILE=PocketMine-MP.phar
-) else (
-	if exist src\pocketmine\PocketMine.php (
-		set IMAGICALMINE_FILE=src\pocketmine\PocketMine.php
+		set POCKETMINE_FILE=PocketMine-MP.phar
 	) else (
-		echo "error> There was an error in starting ImagicalMine. Check that this is either a file named ImagicalMine.phar or PocketMine-MP.phar or a src folder, or try reinstalling ImagicalMine with instructions at imagicalmine.net."
-		pause
-		exit 7
+	    if exist src\pocketmine\PocketMine.php (
+	        set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+		) else (
+		    echo "[ERROR] Couldn't find a valid ImagicalMine installation."
+		    pause
+		    exit 8
+		    )
+	    )
 	)
 )
+
 if exist bin\mintty.exe (
-	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "ImagicalMine" -i bin/imagicalmine.ico -w max %PHP_BINARY% %IMAGICALMINE_FILE% --enable-ansi %*
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "Tesseract" -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
 ) else (
-	%PHP_BINARY% -c bin\php %IMAGICALMINE_FILE% %*
+	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
 )
