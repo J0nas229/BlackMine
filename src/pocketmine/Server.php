e4c4fa23e09698d4f1fde73aec352cbd7799a810
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-04-23 09:13:34 +0200

    cleanup

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1824,7 +1824,7 @@ class Server {
 			Biome::init();
 			Effect::init();
 			Attribute::init();
-	//		EnchantmentLevelTable::init();
+	                //EnchantmentLevelTable::init();
 			//Color::init();
 			$this->craftingManager = new CraftingManager();
 
