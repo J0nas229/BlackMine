e911b1e142215814f073153a5ef0f486685aa052
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-04-24 13:39:51 +0200

    Add files via upload

--- src/pocketmine/block/WoodenButton.php
+++ src/pocketmine/block/WoodenButton.php
@@ -2,20 +2,22 @@
 
 /*
  *
- *  _____   _____   __   _   _   _____  __    __  _____
- * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
- * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
- * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
- * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
- * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
+ *    _______                                _
+ *   |__   __|                              | |
+ *      | | ___  ___ ___  ___ _ __ __ _  ___| |_
+ *      | |/ _ \/ __/ __|/ _ \  __/ _` |/ __| __|
+ *      | |  __/\__ \__ \  __/ | | (_| | (__| |_
+ *      |_|\___||___/___/\___|_|  \__,_|\___|\__|
+ *
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author iTX Technologies
- * @link https://itxtech.org
+ * @author Tessetact Team
+ * @link http://www.github.com/TesseractTeam/Tesseract
+ * 
  *
  */
 
@@ -27,22 +29,22 @@ use pocketmine\level\sound\ButtonClickSound;
 use pocketmine\math\Vector3;
 use pocketmine\Player;
 
-class WoodenButton extends Flowable{
+class WoodenButton extends Transparent{
 	protected $id = self::WOODEN_BUTTON;
-
-	public $isActivated = false;
 
 	public function __construct($meta = 0){
 		$this->meta = $meta;
 	}
-
+	public function isSolid(){
+    	return false;
+	}
 	public function onUpdate($type){
 		if($type == Level::BLOCK_UPDATE_SCHEDULED){
 			if($this->isActivated()) {
 				$this->meta ^= 0x08;
 				$this->getLevel()->setBlock($this, $this, true, false);
 				$this->getLevel()->addSound(new ButtonClickSound($this));
-
+				$this->deactivate();
 			}
 			return Level::BLOCK_UPDATE_SCHEDULED;
 		}
@@ -67,7 +69,49 @@ class WoodenButton extends Flowable{
 		return false;
 	}
 
-	public function getName(){
+	public function deactivate(array $ignore = []){
+		parent::deactivate($ignore = []);
+		$faces = [
+			0 => 1,
+			1 => 0,
+			2 => 3,
+			3 => 2,
+			4 => 5,
+			5 => 4,
+		];
+		$side = $this->meta;
+		if($this->isActivated()) $side ^= 0x08;
+
+		$block = $this->getSide($faces[$side])->getSide(Vector3::SIDE_UP);
+
+		if($side != 1){
+		}
+
+	}
+
+	public function activate(array $ignore = []){
+		parent::activate($ignore = []);
+		$faces = [
+				0 => 1,
+				1 => 0,
+				2 => 3,
+				3 => 2,
+				4 => 5,
+				5 => 4,
+		];
+		
+		$side = $this->meta;
+		if($this->isActivated()) $side ^= 0x08;
+
+		$block = $this->getSide($faces[$side])->getSide(Vector3::SIDE_UP);
+
+		if($side != 1){
+			$block = $this->getSide($faces[$side], 2);
+		}
+
+	}
+
+	public function getName() : string{
 		return "Wooden Button";
 	}
 
@@ -79,6 +123,7 @@ class WoodenButton extends Flowable{
 		if($this->isActivated()){
 			$this->meta ^= 0x08;
 			$this->getLevel()->setBlock($this, $this, true, false);
+			$this->deactivate();
 		}
 		$this->getLevel()->setBlock($this, new Air(), true, false);
 	}
@@ -105,6 +150,7 @@ class WoodenButton extends Flowable{
 			$this->meta ^= 0x08;
 			$this->getLevel()->setBlock($this, $this, true, false);
 			$this->getLevel()->addSound(new ButtonClickSound($this));
+			$this->activate();
 			$this->getLevel()->scheduleUpdate($this, 30);
 		}
 		return true;
