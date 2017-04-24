8af5f7d3132ec9b5e526afb9f93d79befc15037a
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-04-24 07:31:53 +0200

    oops

--- src/pocketmine/network/RakLibInterface.php
+++ src/pocketmine/network/RakLibInterface.php
@@ -172,8 +172,8 @@ class RakLibInterface implements ServerInstance, AdvancedSourceInterface{
 
 		$this->interface->sendOption("name",
 			"MCPE;" . rtrim(addcslashes($name, ";"), '\\') . ";" .
-			Info::CURRENT_PROTOCOL . ";" .
-			Info::MINECRAFT_VERSION_NETWORK . ";" .
+			ProtocolInfo::CURRENT_PROTOCOL . ";" .
+			ProtocolInfo::MINECRAFT_VERSION_NETWORK . ";" .
 			$info->getPlayerCount() . ";" .
 			$info->getMaxPlayerCount()
 		);
