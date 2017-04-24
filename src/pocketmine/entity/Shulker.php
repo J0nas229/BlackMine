d8759be9173e109fb1ccd3aeae1e591ae0e49522
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-04-24 13:57:19 +0200

    Add files via upload

--- src/pocketmine/entity/Shulker.php
+++ src/pocketmine/entity/Shulker.php
@@ -1,55 +1,66 @@
 <?php
 
 /*
- *   ____  _            _      _       _     _
- *  |  _ \| |          | |    (_)     | |   | |
- *  | |_) | |_   _  ___| |     _  __ _| |__ | |_
- *  |  _ <| | | | |/ _ \ |    | |/ _` | '_ \| __|
- *  | |_) | | |_| |  __/ |____| | (_| | | | | |_
- *  |____/|_|\__,_|\___|______|_|\__, |_| |_|\__|
- *                                __/ |
- *                               |___/
+ *
+ *  _____   _____   __   _   _   _____  __    __  _____
+ * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
+ * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
+ * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
+ * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
+ * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author BlueLightJapan Team
- * 
-*/
-
+ * @author iTX Technologies
+ * @link https://itxtech.org
+ *
+ */
 
 namespace pocketmine\entity;
+
 use pocketmine\network\protocol\AddEntityPacket;
 use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\item\Item as ItemItem;
 
-class Shulker extends Monster{
-	const NETWORK_ID = 54;
+class Shulker extends Monster {
+    const NETWORK_ID = 54;
 
-	public $width = 1;
-	public $length = 1;
-	public $height = 1;
-	public $maxhealth = 30;
+    public $width = 0.5;
+    public $length = 0.9;
+    public $height = 1.0;
 
-	public function getName(){
-		return "Stray";
-	}
-	
-	public function spawnTo(Player $player){
-		$pk = new AddEntityPacket();
-		$pk->eid = $this->getId();
-		$pk->type = self::NETWORK_ID;
-		$pk->x = $this->x;
-		$pk->y = $this->y;
-		$pk->z = $this->z;
-		$pk->speedX = $this->motionX;
-		$pk->speedY = $this->motionY;
-		$pk->speedZ = $this->motionZ;
-		$pk->yaw = $this->yaw;
-		$pk->pitch = $this->pitch;
-		$pk->metadata = $this->dataProperties;
-		$player->dataPacket($pk);
+    public $dropExp = [1, 4];
 
-		parent::spawnTo($player);
-	}
+    public function getName(): string {
+        return "Shulker";
+    }
+
+    public function spawnTo(Player $player) {
+        $pk = new AddEntityPacket();
+        $pk->eid = $this->getId();
+        $pk->type = Shulker::NETWORK_ID;
+        $pk->x = $this->x;
+        $pk->y = $this->y;
+        $pk->z = $this->z;
+        $pk->speedX = $this->motionX;
+        $pk->speedY = $this->motionY;
+        $pk->speedZ = $this->motionZ;
+        $pk->yaw = $this->yaw;
+        $pk->pitch = $this->pitch;
+        $pk->metadata = $this->dataProperties;
+        $player->dataPacket($pk);
+        parent::spawnTo($player);
+    }
+
+    public function getDrops() {
+        $drops = [];
+        if ($this->lastDamageCause instanceof EntityDamageByEntityEvent and $this->lastDamageCause->getEntity() instanceof Player) {
+            if (mt_rand(0, 1) === 1) $drops[] = ItemItem::get(ItemItem::SHULKER_SHELL, 0, 1);
+        }
+        return $drops;
+    }
 }
