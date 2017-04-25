7dad22fd692cc345164cda1d0340b4d0bf282460
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-04-25 10:06:12 +0200

    Add files via upload

--- src/pocketmine/resourcepacks/ZippedResourcePack.php
+++ src/pocketmine/resourcepacks/ZippedResourcePack.php
@@ -8,15 +8,6 @@
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
  * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
- *  _____            _               _____           
- * / ____|          (_)             |  __ \          
- *| |  __  ___ _ __  _ ___ _   _ ___| |__) | __ ___  
- *| | |_ |/ _ \ '_ \| / __| | | / __|  ___/ '__/ _ \ 
- *| |__| |  __/ | | | \__ \ |_| \__ \ |   | | | (_) |
- * \_____|\___|_| |_|_|___/\__, |___/_|   |_|  \___/ 
- *                         __/ |                    
- *                        |___/                     
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -28,20 +19,25 @@
  *
 */
 
-/*
- *Zip材质包加载接口
- *
-*/
 
 namespace pocketmine\resourcepacks;
 
 
 class ZippedResourcePack implements ResourcePack{
 
+	/**
+	 * Performs basic validation checks on a resource pack's manifest.json.
+	 * TODO: add more manifest validation
+	 *
+	 * @param \stdClass $manifest
+	 * @return bool
+	 */
 	public static function verifyManifest(\stdClass $manifest){
 		if(!isset($manifest->format_version) or !isset($manifest->header) or !isset($manifest->modules)){
 			return false;
 		}
+
+		//Right now we don't care about anything else, only the stuff we're sending to clients.
 		return
 			isset($manifest->header->description) and
 			isset($manifest->header->name) and
@@ -59,31 +55,42 @@ class ZippedResourcePack implements ResourcePack{
 	/** @var string */
 	protected $sha256 = null;
 
+	/** @var resource */
+	protected $fileResource;
 
+	/**
+	 * @param string $zipPath Path to the resource pack zip
+	 */
 	public function __construct(string $zipPath){
 		$this->path = $zipPath;
 
 		if(!file_exists($zipPath)){
-			throw new \InvalidArgumentException("无法打开材质包 $zipPath: 文件夹无法打开");
+			throw new \InvalidArgumentException("Could not open resource pack $zipPath: file not found");
 		}
 
 		$archive = new \ZipArchive();
 		if(($openResult = $archive->open($zipPath)) !== true){
-			throw new \InvalidStateException("打开 $zipPath时遇到ZipArchive错误 $openResult");
+			throw new \InvalidStateException("Encountered ZipArchive error code $openResult while trying to open $zipPath");
 		}
 
 		if(($manifestData = $archive->getFromName("manifest.json")) === false){
-			throw new \InvalidStateException("无法加载材质包 $zipPath: 找不到主类");
+			throw new \InvalidStateException("Could not load resource pack from $zipPath: manifest.json not found");
 		}
 
 		$archive->close();
 
 		$manifest = json_decode($manifestData);
 		if(!self::verifyManifest($manifest)){
-			throw new \InvalidStateException("无法加载材质包 $zipPath: 主类错误或不完整");
+			throw new \InvalidStateException("Could not load resource pack from $zipPath: manifest.json is invalid or incomplete");
 		}
 
 		$this->manifest = $manifest;
+
+		$this->fileResource = fopen($zipPath, "rb");
+	}
+
+	public function __destruct(){
+		fclose($this->fileResource);
 	}
 
 	public function getPackName() : string{
@@ -104,12 +111,16 @@ class ZippedResourcePack implements ResourcePack{
 
 	public function getSha256(bool $cached = true) : string{
 		if($this->sha256 === null or !$cached){
-			$this->sha256 = openssl_digest(file_get_contents($this->path), "sha256", true);
+			$this->sha256 = hash_file("sha256", $this->path, true);
 		}
 		return $this->sha256;
 	}
 
 	public function getPackChunk(int $start, int $length) : string{
-		return substr(file_get_contents($this->path), $start, $length);
+		fseek($this->fileResource, $start);
+		if(feof($this->fileResource)){
+			throw new \RuntimeException("Requested a resource pack chunk with invalid start offset");
+		}
+		return fread($this->fileResource, $length);
 	}
 }
