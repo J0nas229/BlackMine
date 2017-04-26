f291eaee9c60f523a79f889d73cfaad2168a19f9
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-04-26 18:00:52 +0200

    only one version not multi

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -34,8 +34,8 @@ interface Info{
 	 * Actual Minecraft: PE protocol version
 	 */
 	const CURRENT_PROTOCOL = 107;
-	const ACCEPTED_PROTOCOLS = [105,106,107];	
-	const MINECRAFT_VERSION = ["v1.0.5","v1.0.6","v1.0.7"];
+	const ACCEPTED_PROTOCOLS = [107];	
+	const MINECRAFT_VERSION = ["v1.0.7"];
 	const MINECRAFT_VERSION_NETWORK = "1.0.7";
 
 	const LOGIN_PACKET = 0x01;
