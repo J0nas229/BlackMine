552efa82d089a9e958f7127852ea7b0b37a94b7c
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-04-27 08:40:57 +0200

    update try to fix redstone

--- src/pocketmine/entity/EnderPearl.php
+++ src/pocketmine/entity/EnderPearl.php
@@ -1,7 +1,25 @@
 <?php
 
-namespace pocketmine\entity;
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
 
+namespace pocketmine\entity;
 
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\level\Level;
@@ -9,70 +27,67 @@ use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\protocol\AddEntityPacket;
 use pocketmine\Player;
 
-class EnderPearl extends Projectile {
-    const NETWORK_ID = 87;
+class EnderPearl extends Projectile{
 
-    public $width = 0.25;
-    public $length = 0.25;
-    public $height = 0.25;
+	const NETWORK_ID = 87;
 
-    protected $gravity = 0.03;
-    protected $drag = 0.01;
-    protected $player;
+	public $width = 0.25;
+	public $length = 0.25;
+	public $height = 0.25;
 
-    private $hasTeleportedShooter = false;
+	protected $gravity = 0.03;
+	protected $drag = 0.01;
 
-    public function __construct(Level $level, CompoundTag $nbt, Entity $shootingEntity = null) {
-        parent::__construct($level, $nbt, $shootingEntity);
-    }
+	private $hasTeleportedShooter = false;
 
-    public function teleportShooter() {
-        if (!$this->hasTeleportedShooter) {
-            $this->hasTeleportedShooter = true;
-            if ($this->isAlive()) {
-                if ($this->shootingEntity instanceof Player and $this->y > 0) {
-                    $this->shootingEntity->attack(5, new EntityDamageEvent($this->shootingEntity, EntityDamageEvent::CAUSE_FALL, 5));
-                    $this->shootingEntity->teleport($this->getPosition());
-                }
+	public function __construct(Level $level, CompoundTag $nbt, Entity $shootingEntity = null){
+		parent::__construct($level, $nbt, $shootingEntity);
+	}
 
-                $this->kill();
-            }
-        }
-    }
+	public function teleportShooter(){
+		if(!$this->hasTeleportedShooter){
+			$this->hasTeleportedShooter = true;
+			if($this->shootingEntity instanceof Player and $this->y > 0){
+				$this->shootingEntity->attack(5, new EntityDamageEvent($this->shootingEntity, EntityDamageEvent::CAUSE_FALL, 5));
+				$this->shootingEntity->teleport($this->getPosition());
+			}
 
-    public function onUpdate($currentTick) {
-        if ($this->closed) {
-            return false;
-        }
+			$this->kill();
+		}
+	}
 
-        $this->timings->startTiming();
+	public function onUpdate($currentTick){
+		if($this->closed){
+			return false;
+		}
 
-        $hasUpdate = parent::onUpdate($currentTick);
+		$this->timings->startTiming();
 
-        if ($this->age > 1200 or $this->isCollided) {
-            $this->teleportShooter();
-            $hasUpdate = true;
-        }
+		$hasUpdate = parent::onUpdate($currentTick);
 
-        $this->timings->stopTiming();
+		if($this->age > 1200 or $this->isCollided){
+			$this->teleportShooter();
+			$hasUpdate = true;
+		}
 
-        return $hasUpdate;
-    }
+		$this->timings->stopTiming();
 
+		return $hasUpdate;
+	}
 
-    public function spawnTo(Player $player) {
-        $pk = new AddEntityPacket();
-        $pk->type = self::NETWORK_ID;
-        $pk->eid = $this->getId();
-        $pk->x = $this->x;
-        $pk->y = $this->y;
-        $pk->z = $this->z;
-        $pk->speedX = $this->motionX;
-        $pk->speedY = $this->motionY;
-        $pk->speedZ = $this->motionZ;
-        $pk->metadata = $this->dataProperties;
-        $player->dataPacket($pk);
+	public function spawnTo(Player $player){
+		$pk = new AddEntityPacket();
+		$pk->type = EnderPearl::NETWORK_ID;
+		$pk->eid = $this->getId();
+		$pk->x = $this->x;
+		$pk->y = $this->y;
+		$pk->z = $this->z;
+		$pk->speedX = $this->motionX;
+		$pk->speedY = $this->motionY;
+		$pk->speedZ = $this->motionZ;
+		$pk->metadata = $this->dataProperties;
+		$player->dataPacket($pk);
+		parent::spawnTo($player);
+	}
 
-        parent::spawnTo($player);
-    }
 }
--- src/pocketmine/entity/Horse.php
+++ src/pocketmine/entity/Horse.php
@@ -2,24 +2,27 @@
 
 /*
  *
- *    _______                                _
- *   |__   __|                              | |
- *      | | ___  ___ ___  ___ _ __ __ _  ___| |_
- *      | |/ _ \/ __/ __|/ _ \  __/ _` |/ __| __|
- *      | |  __/\__ \__ \  __/ | | (_| | (__| |_
- *      |_|\___||___/___/\___|_|  \__,_|\___|\__|
- *
+ *  _____            _               _____           
+ * / ____|          (_)             |  __ \          
+ *| |  __  ___ _ __  _ ___ _   _ ___| |__) | __ ___  
+ *| | |_ |/ _ \ '_ \| / __| | | / __|  ___/ '__/ _ \ 
+ *| |__| |  __/ | | | \__ \ |_| \__ \ |   | | | (_) |
+ * \_____|\___|_| |_|_|___/\__, |___/_|   |_|  \___/ 
+ *                         __/ |                    
+ *                        |___/                     
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author Tesseract Team
- * @link http://www.github.com/TesseractTeam/Tesseract
- * 
+ * @author GenisysPro
+ * @link https://github.com/GenisysPro/GenisysPro
  *
- */
+ *
+*/
+
+/* TODO: 骑马 */
 
 namespace pocketmine\entity;
 
@@ -27,30 +30,47 @@ use pocketmine\Player;
 use pocketmine\network\protocol\AddEntityPacket;
 use pocketmine\network\protocol\MobArmorEquipmentPacket;
 
-class Horse extends Living {
+class Horse extends Living{
 
-    const NETWORK_ID = 23;
+	const NETWORK_ID = 23;
 
-    public function getName(): string {
-        return "Horse";
-    }
+	public function getName() : string{
+		return "Horse";
+	}
+	
+	public function setChestPlate($id){
+		/*	
+		416, 417, 418, 419 only
+		*/
+		$pk = new MobArmorEquipmentPacket();
+		$pk->eid = $this->getId();
+		$pk->slots = [
+		ItemItem::get(0,0),
+		ItemItem::get($id,0),
+		ItemItem::get(0,0),
+		ItemItem::get(0,0)
+		];
+		foreach($this->level->getPlayers() as $player){
+			$player->dataPacket($pk);
+		}
+	}
+	
+	public function spawnTo(Player $player){
+		$pk = new AddEntityPacket();
+		$pk->eid = $this->getId();
+		$pk->type = self::NETWORK_ID;
+		$pk->x = $this->x;
+		$pk->y = $this->y;
+		$pk->z = $this->z;
+		$pk->speedX = $this->motionX;
+		$pk->speedY = $this->motionY;
+		$pk->speedZ = $this->motionZ;
+		$pk->yaw = $this->yaw;
+		$pk->pitch = $this->pitch;
+		$pk->metadata = $this->dataProperties;
+		$player->dataPacket($pk);
 
-    public function spawnTo(Player $player) {
-        $pk = new AddEntityPacket();
-        $pk->eid = $this->getId();
-        $pk->type = self::NETWORK_ID;
-        $pk->x = $this->x;
-        $pk->y = $this->y;
-        $pk->z = $this->z;
-        $pk->speedX = $this->motionX;
-        $pk->speedY = $this->motionY;
-        $pk->speedZ = $this->motionZ;
-        $pk->yaw = $this->yaw;
-        $pk->pitch = $this->pitch;
-        $pk->metadata = $this->dataProperties;
-        $player->dataPacket($pk);
-
-        parent::spawnTo($player);
-    }
+		parent::spawnTo($player);
+	}
 
 }
