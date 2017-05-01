423140a24fce1a63a4cceeed22e45956d60f421c
jdongel2004 <jdongel2002@gmail.com>
2017-05-01 17:48:31 +0900

    Can I do this..?

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -95,9 +95,9 @@ namespace pocketmine {
     use pocketmine\utils\Utils;
     use pocketmine\wizard\Installer;
 
-    const VERSION = "3.0";
+    const VERSION = "1.0 beta";
     const API_VERSION = "3.0.1";
-    const CODENAME = "Imagical";
+    const CODENAME = "BlackMine";
     const MINECRAFT_VERSION = "v1.0.7 alpha";
     const MINECRAFT_VERSION_NETWORK = "1.0.7";
 
