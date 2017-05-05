a7e12d1bdb88a4062931ab852c483476e4a45857
jdongel2004 <jdongel2002@gmail.com>
2017-05-05 12:46:42 +0900

    Support v1.0.8

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -16,8 +16,8 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author GenisysPro
- * @link https://github.com/GenisysPro/GenisysPro
+ * @author BlackLight-NL
+ * @link https://github.com/BlackLight-NL/BlackMine
  *
  *
 */
@@ -33,10 +33,10 @@ interface Info{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	const CURRENT_PROTOCOL = 107;
-	const ACCEPTED_PROTOCOLS = [107];	
-	const MINECRAFT_VERSION = ["v1.0.7"];
-	const MINECRAFT_VERSION_NETWORK = "1.0.7";
+	const CURRENT_PROTOCOL = 108;
+	const ACCEPTED_PROTOCOLS = [107,108];	
+	const MINECRAFT_VERSION = ["v1.0.7","v1.0.8];
+	const MINECRAFT_VERSION_NETWORK = "1.0.8";
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
