b6cf08f073b7045567781f4fb84b57d16104dc20
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-05-08 14:52:44 +0200

    Update Server.php

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1455,7 +1455,7 @@ class Server{
 			"retryTimes" => $this->getAdvancedProperty("dserver.retry-times", 3),
 			"serverList" => explode(";", $this->getAdvancedProperty("dserver.server-list", ""))
 		];
-		$this->getLogger()->setWrite(!$this->getAdvancedProperty("server.disable-log", false));
+		//$this->getLogger()->setWrite(!$this->getAdvancedProperty("server.disable-log", false));
 		$this->asyncChunkRequest = $this->getAdvancedProperty("server.async-chunk-request", true);
 		$this->limitedCreative = $this->getAdvancedProperty("server.limited-creative", true);
 		$this->chunkRadius = $this->getAdvancedProperty("player.chunk-radius", -1);
